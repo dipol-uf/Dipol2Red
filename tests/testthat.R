604faf14200481b9e063435d84baa767aa2e5e12
Ilia Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2018-12-14 16:13:22 +0200

    License modifications

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,6 +1,8 @@
 #   MIT License
 #
-#   Copyright(c) 2018 Ilia Kosenkov [ilia.kosenkov.at.gm@gmail.com]
+#   Copyright(c) 2018
+#   Ilia Kosenkov [ilia.kosenkov.at.gm@gmail.com],
+#   Vilppu Piirola
 #
 #   Permission is hereby granted, free of charge, to any person obtaining a copy
 #   of this software and associated documentation files(the "Software"), to deal
