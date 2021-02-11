6e75b66063f3590c6abdc4c5f65081638c57fa2b
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2021-02-11 23:53:29 +0200

    Error handling

--- R/load_from_legacy_descriptor.R
+++ R/load_from_legacy_descriptor.R
@@ -3,12 +3,6 @@
 #' @param root Root for the files in the descriptor.
 #' @export
 load_from_legacy_descriptor <- function(desc, root = ".") {
-  # data <- desc %>%
-  #     map(extract2, "File") %>%
-  #     map_if(~nzchar(root), ~ fs::path(root, .x)) %>%
-  #     map(read_csv, col_types = cols()) %>%
-  #     map(~set_names(.x, fix_names(names(.x))))
-
   data <-
     desc %>%
     mutate(
@@ -23,13 +17,35 @@ load_from_legacy_descriptor <- function(desc, root = ".") {
       ) %>%
       as_list_of
     )
-    
 
-  # walk2(data, desc, function(obs, des) {
-  #         if (vec_size(obs) != des$Count)
-  #             warning(paste0("Read number of observations (", nrow(obs), ") ",
-  #                 "does not match expected number (", des$Count, ")."))
-  #         })
+  with(
+    data,
+    map2_lgl(
+        Count,
+        Data,
+        ~.x != vec_size(.y)
+    )
+  ) %>%
+  which -> invalid
 
-  # return(data)
+  if (vec_size(invalid) > 0L) {
+    msg <- err_idx(invalid, "Data mismatch found at position(s):")
+    abort(
+      paste(
+        "Read data does not match its descriptor.",
+        paste(
+          err_cross(),
+          msg
+        ),
+        paste(
+          err_info(),
+          "Make sure descriptor contains up to date information."
+        ),
+        sep = "\n "
+      ),
+      class = "d2r_io_failed"
+    )
+  }
+
+  data
 }
