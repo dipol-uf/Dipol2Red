da67f5fc37c40d4be5e1a0012a110f75b645f118
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2021-02-12 11:50:50 +0200

    Refactoring 'fix_names'

--- R/fix_names.R
+++ R/fix_names.R
@@ -31,43 +31,81 @@ utils::globalVariables(vctrs::vec_c("Obj", "Name", "Src", "Result", "Desc"))
 #' @return R-compatible names suitable for further work
 #' @export
 fix_names <- function(x) {
-    UseMethod("fix_names")
+  UseMethod("fix_names")
 }
 
 #' @rdname fix_names
 #' @export
 fix_names.character <- function(x) {
-    x %>%
-        str_match(regex("(jd|mjd|date|filter|ref|obj)(\\d+)?(?:\\s*:\\s*(.*)$)?", ignore_case = TRUE)) %>%
-        as_tibble(.name_repair = ~vec_c("Match", "Name", "Id", "Desc"))  %>%
-        mutate(
-            Desc = if_else(
-                str_detect(Desc, "[Mm]agnitude(?:\\s*\\([Cc]entroid\\))?") | is.na(Desc),
-                "",
-                paste0("_(", str_replace(Desc, "\\s+", "_"), ")")),
-            Name = tolower(Name),
-            Src = x,
-            Id = if_else(nzchar(Id) & !is.na(Id), paste0("_", Id), "")) %>%
-        mutate(
-            Name = case_when(
-                vec_in(Name, vec_c("jd", "date", "mjd")) ~ "JD",
-                Name == "obj" ~ "Obj",
-                Name == "ref" ~ "Ref",
-                Name == "filter" ~ "Filter",
-                TRUE ~ Src)) %>%
-        mutate(Result = paste0(Name, Id, Desc)) %>%
-        pull(Result)
+  x %>%
+    str_match(
+      regex(
+        "(jd|mjd|date|filter|ref|obj)(\\d+)?(?:\\s*:\\s*(.*)$)?",
+        ignore_case = TRUE
+      )
+    ) %>%
+    as_tibble(.name_repair = "minimal") %>%
+    set_names(c("Match", "Name", "Id", "Desc")) %>%
+    mutate(
+      Desc = if_else(
+        str_detect(
+          Desc,
+          "[Mm]agnitude(?:\\s*\\([Cc]entroid\\))?"
+        ) |
+          is.na(Desc),
+        "",
+        paste0("_(", str_replace(Desc, "\\s+", "_"), ")")
+      ),
+      Id = if_else(
+        nzchar(Id) & !is.na(Id),
+        paste0("_", Id),
+        ""
+      ),
+      Name = tolower(Name),
+      Name = case_when(
+        vec_in(Name, vec_c("jd", "date", "mjd")) ~ "JD",
+        Name == "obj" ~ "Obj",
+        Name == "ref" ~ "Ref",
+        Name == "filter" ~ "Filter",
+        TRUE ~ x
+      ),
+      Result = paste0(Name, Id, Desc)
+    ) %>%
+    pull(Result)
 }
 
 #' @rdname fix_names
 #' @export
-fix_names.tbl_df <- function(x)
-    set_names(x, fix_names(names(x)))
+fix_names.tbl_df <- function(x) {
+  set_names(x, fix_names(names(x)))
+}
 
 #' @rdname fix_names
 #' @export
-fix_names.data.frame <- function(x)
-    set_names(x, fix_names(names(x)))
+fix_names.data.frame <- function(x) {
+  set_names(x, fix_names(names(x)))
+}
 
-fix_names.default <- function(x)
-    rlang::abort("Cannot fix names. Object type is not supported.", "Dipol2Red_type_mismatch")
+fix_names.default <- function(x) {
+  abort(
+    paste(
+      err_invalid_arg(),
+      glue(
+        err_cross(),
+        "Cannot fix names of object of type",
+        "`{vec_ptype_abbr(x)}`.",
+        .sep = " "
+      ),
+      glue(
+        err_info(),
+        "Supported types are",
+        "`{vec_ptype_abbr(character())}`,",
+        "`{vec_ptype_abbr(tibble())}`, ",
+        "`{vec_ptype_abbr(data.frame())}`, ",
+        .sep = " "
+      ),
+      sep = "\n "
+    ),
+    class = "d2r_invalid_arg"
+  )
+}
