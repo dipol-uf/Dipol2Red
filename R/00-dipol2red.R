35624af48220f8a2e801f76d07bacb0d805aab02
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2021-02-12 11:20:01 +0200

    Fixing imports

--- R/00-dipol2red.R
+++ R/00-dipol2red.R
@@ -4,23 +4,27 @@
 #' @importFrom vctrs      vec_c vec_in vec_rbind vec_is_empty vec_size
 #' @importFrom vctrs      vec_recycle vec_recycle_common vec_slice vec_cbind
 #' @importFrom vctrs      vec_cast list_of as_list_of vec_cast_common
+#' @importFrom vctrs      vec_assert
 #' 
-#' @importFrom purrr      map imap map_chr keep reduce flatten_chr flatten_dbl discard map_if
-#' @importFrom purrr      walk2 map2_chr map_at some map_dfr compose
+#' @importFrom purrr      map imap map_chr keep reduce flatten_chr flatten_dbl
+#' @importFrom purrr      walk2 map2_chr map_at some map_dfr compose map_if
+#' @importFrom purrr      discard map2_lgl imap_dfr
 #'
-#' @importFrom stringr    str_detect regex str_subset str_split str_match str_extract
-#' @importFrom stringr    str_replace
+#' @importFrom stringr    str_detect regex str_subset str_split str_match
+#' @importFrom stringr    str_replace str_extract
 #'
-#' @importFrom dplyr      %>% case_when group_vars group_indices arrange group_split
-#' @importFrom dplyr      pull mutate group_by summarise if_else n select tbl_vars slice rename
-#' @importFrom dplyr      transmute group_map is_grouped_df bind_cols bind_rows one_of
-#'
+#' @importFrom dplyr      %>% case_when group_vars group_indices arrange ungroup
+#' @importFrom dplyr      pull mutate group_by summarise if_else n select
+#' @importFrom dplyr      transmute group_map is_grouped_df bind_cols bind_rows
+#' @importFrom dplyr      across one_of tbl_vars slice rename group_split filter
+#' @importFrom dplyr      slice_sample
+#' 
 #' @importFrom forcats    as_factor
 #'
 #' @importFrom rlang      as_name enquo !! !!! is_null as_function enquos
 #' @importFrom rlang      is_empty is_character syms quo is_missing ensym
 #' @importFrom rlang      ensyms quo_get_env quo_get_expr set_names sym abort
-#' @importFrom rlang      is_double is_integer
+#' @importFrom rlang      is_double is_integer list2 names2
 #'
 #' @importFrom utils      glob2rx
 #'
