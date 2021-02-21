efeac4e5cebc8fac7282b5b0d555bae72837eb9d
Ilia <ilia.kosenkov@outlook.com>
2021-02-21 14:23:15 +0200

    Fixing 'process_files' (#8)
    * Fixed 'process_path'
    
    * Fixing import && defining default value

--- R/process_path.R
+++ R/process_path.R
@@ -5,37 +5,35 @@
 #'
 #' @return A list of tibbles
 #' @export
-process_files <- function(path,  by = 4L) {
+process_files <- function(path = "*.csv", by = 4L) {
   files <- Sys.glob(path)
   files <- rlang::set_names(files, fs::path_file(files))
 
   proc <- function(loc_path) {
     # Currently read only csv
-    #fs::path_ext(pth) -> ext
     loc_path %>%
-        readr::read_csv(col_types = readr::cols(), comment = "#") %>%
-        fix_names -> data
+      readr::read_csv(col_types = readr::cols(), comment = "#") %>%
+      fix_names() -> data
 
     nms <- rlang::names2(data)
-    obj_names <- stringr::str_subset(nms, "^Obj_\\d+$")
     ref_names <- stringr::str_subset(nms, "^Ref_\\d+$")
 
     ref_names %>%
       purrr::keep(
-        ~sqrt(sum(data[[.x]] ^ 2) / vctrs::vec_size(data)) > .Machine$double.eps
+        ~ sqrt(sum(data[[.x]]^2) / vctrs::vec_size(data)) > .Machine$double.eps
       ) %>%
       purrr::map(
-        ~readr::parse_integer(stringr::str_extract(.x, "(?<=_)\\d+$"))
+        ~ readr::parse_integer(stringr::str_extract(.x, "(?<=_)\\d+$"))
       ) -> non_empty_ref_id
 
     if (!vctrs::vec_is_empty(non_empty_ref_id)) {
-
       q <- non_empty_ref_id %>%
         rlang::set_names(glue::glue("Obj_{.}")) %>%
         purrr::map(
-          ~rlang::quo(
+          ~ rlang::quo(
             !!rlang::sym(glue::glue("Obj_{.x}")) -
-              !!rlang::sym(glue::glue("Ref_{.x}")))
+              !!rlang::sym(glue::glue("Ref_{.x}"))
+          )
         )
 
       dplyr::mutate(data, !!!q) -> data
@@ -55,10 +53,10 @@ process_files <- function(path,  by = 4L) {
       ) -> result_full
 
     data %>%
-      dplyr::mutate(Id = factor((1:n() - 1L) %/% (4L * by))) %>%
+      dplyr::mutate(Id = factor((1:dplyr::n() - 1L) %/% (4L * by))) %>%
       dplyr::group_by(.data$Id) %>%
       fsigma_2(date_col = "JD", obs_col = !!nm) %>%
-      select(
+      dplyr::select(
         .data$Id, .data$JD, .data$Px, .data$Py,
         .data$P, .data$SG, .data$A, .data$SG_A,
         .data$N, .data$Ratio
@@ -69,9 +67,8 @@ process_files <- function(path,  by = 4L) {
       Individual = result_individual
     ) %>%
       purrr::map(dplyr::mutate, JDF = .data$JD - floor(.data$JD))
-
   }
 
   files %>%
-      purrr::map(proc)
+    purrr::map(proc)
 }
